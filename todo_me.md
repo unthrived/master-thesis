2098b6963e729c9783612e9dad6fbeb40e3ccf89
unthrived <junjie.ji@outlook.com>
2023-06-27 15:04:20 +0200

    more results

---
+++ todo_me.md
@@ -0,0 +1,1 @@
+change read data from methods
--- toolbox/methods.py
+++ toolbox/methods.py
@@ -115,11 +115,16 @@ def read_data(
                     'metadata': st_epoch.metadata[st_epoch.metadata['nrep'] == repetition_index]
                     })
             else:
-                all_st_rawdata.append({'epoch_dat': st_epoch.get_data(), 'metadata': st_epoch.metadata})
+                all_st_rawdata.append({
+                    'epoch_dat': st_epoch.get_data() , 
+                    'metadata': st_epoch.metadata
+                    })
     if task == 'main':
         return all_epochs, all_rawdata
-    else:
+        # return all_rawdata
+    if task == 'stim':
         return all_st_epochs, all_st_rawdata
+        # return all_st_rawdata
 
 def read_data_repetitions(
         repetitions=True,
@@ -129,6 +134,7 @@ def read_data_repetitions(
         amount_of_subjects = 1,
         task = 'main'
         ):
+    '''Reading data based on repetitions'''
     if amount_of_subjects < 6:
         subjs_list = ['s{:02d}'.format(i) for i in range(1, amount_of_subjects+1)] 
     elif amount_of_subjects > 26:
@@ -197,14 +203,96 @@ def read_data_repetitions(
                 })
                 all_st_rawdata.append({'epoch_dat': st_epoch.get_data(), 'metadata': st_epoch.metadata})
     if task == 'main':
-        return all_epochs, all_rawdata
-    else:
-        return all_st_epochs, all_st_rawdata
+        return all_rawdata
+    if task == 'stim':
+        return all_st_rawdata
 
+
+def read_data_repetitions_decision(
+        repetitions=True,
+        repetition_index=1,
+        decision=0,
+        resample=False,
+        resample_frequency = 20,
+        amount_of_subjects = 1,
+        task = 'main'
+        ):
+    '''Reading data based on repetitions and last decision'''
+    if amount_of_subjects < 6:
+        subjs_list = ['s{:02d}'.format(i) for i in range(1, amount_of_subjects+1)] 
+    elif amount_of_subjects > 26:
+        amount_of_subjects = 26
+        subjs_list_5 = ['s{:02d}'.format(i) for i in range(1, 6)]
+        subjs_list = ['s{:02d}'.format(i) for i in range(7, amount_of_subjects+2)]
+        subjs_list = subjs_list_5 + subjs_list
+    else:
+        subjs_list_5 = ['s{:02d}'.format(i) for i in range(1, 6)]
+        subjs_list = ['s{:02d}'.format(i) for i in range(7, amount_of_subjects+2)]
+        subjs_list = subjs_list_5 + subjs_list
+
+    path = 'Cond_CJ_EEG'
+
+    epochs = []
+    all_epochs = []
+    all_rawdata = []
+    all_st_epochs = []
+    all_st_rawdata = []
+    for subject_id in subjs_list:
+        preproc_path = os.path.join(path, subject_id)
+
+        if task == 'main':
+            epoch = mne.read_epochs(os.path.join(preproc_path, 'main_epo.fif'), verbose=False)
+            
+            if resample: 
+                print('Frequency before:', epoch.info['sfreq'])
+                epoch = epoch.resample(resample_frequency)
+                print('Frequency after:' ,epoch.info['sfreq'])
+
+            # epochs.append(epoch.average())
+            all_epochs.append(epoch)
+            if repetitions:
+                if repetition_index == 0:
+                    repetition_values = [0]  # Use repetition 0
+                else:
+                    repetition_values = [1, 2]  # Use repetitions 1 and 2
+                all_rawdata.append({
+                    'epoch_dat': epoch.get_data()[np.isin(epoch.metadata['nrep'], repetition_values), :, :],
+                    'metadata': epoch.metadata[np.isin(epoch.metadata['nrep'], repetition_values)]
+                })
+            else:
+                all_rawdata.append({
+                    'epoch_dat': epoch.get_data(), 
+                    'metadata': epoch.metadata
+                    })
+            
+        if task == 'stim':
+        
+            st_epoch = mne.read_epochs(os.path.join(preproc_path, 'mainstim_epo.fif'), verbose=False)
+            # print(st_epoch.info['sfreq'])
+            if resample: 
+                print('Frequency before:', st_epoch.info['sfreq'])
+                st_epoch = st_epoch.resample(resample_frequency)
+                print('Frequency after:' ,st_epoch.info['sfreq'])
+                
+            all_st_epochs.append(st_epoch)
+            if repetitions:
+                if repetition_index == 0:
+                    repetition_values = [0]  # Use repetition 0
+                else:
+                    repetition_values = [1, 2]  # Use repetitions 1 and 2
+                all_st_rawdata.append({
+                    'epoch_dat': st_epoch.get_data()[np.isin(st_epoch.metadata['nrep'], repetition_values), :, :],
+                    'metadata': st_epoch.metadata[np.isin(st_epoch.metadata['nrep'], repetition_values)]
+                })
+                all_st_rawdata.append({'epoch_dat': st_epoch.get_data(), 'metadata': st_epoch.metadata})
+    if task == 'main':
+        return all_rawdata
+    if task == 'stim':
+        return all_st_rawdata
 
 
 def train_stim_ori(all_st_rawdata, raw_predicts = False):
-    '''Train procedure used for forward encoding model'''
+    '''Train procedure used for forward encoding model with st data'''
     nSubj = len(all_st_rawdata)
     preds = [None] * nSubj
     G = [None] * nSubj
@@ -254,60 +342,7 @@ def train_stim_ori(all_st_rawdata, raw_predicts = False):
         Xhat_centeredmean = np.mean( Xhat_centeredmean, axis = 2)
         
         return Xhat_centeredmean
-    
-def train_condv(all_rawdata, raw_predicts = False):
-    '''not working'''
-    nSubj = len(all_rawdata)
-    preds = [None] * nSubj
-    G = [None] * nSubj
 
-    for subj in range(nSubj):
-        
-        X = all_rawdata[subj]['epoch_dat']
-        if subj == 1: print(X.shape)
-        X = np.einsum('kji->jik', X)        
-        if subj == 1: print(X.shape)
-        
-        X = np.delete(X, 25, axis=0)
-        all_rawdata[subj]['metadata']['condv'] = 'C'
-        all_rawdata[subj]['metadata'].loc[all_rawdata[subj]['metadata']['cond']== 1, 'condv'] =  'D'
-
-        y = np.array(all_rawdata[subj]['metadata'].condv)
-        phi = y
-        numF, numT, numN = X.shape
-
-        G[subj] = y.copy() 
-        
-        CONDS = np.unique(G[subj])
-        nConds = CONDS.size
-        nfold = 5
-        FoldsIdx = dt.CreateFolds(G[subj], X, nfold)
-        
-        design, sortedesign = dt.stim_features(phi, cfg_stim)
-        
-        Xhat = np.zeros([numC,numN, numT])
-        for it in range(numT):
-            cfg = dict()
-            cfg['cfgE'] = {'gamma': 0.01, 'demean' : True, 'returnPattern' : True}
-            cfg['cfgD'] = {'demean' : 'traindata'}
-            Xhat[:,:,it] = dt.CV_encoder(design, X, it, cfg, FoldsIdx)
-        preds[subj] = Xhat
-    if raw_predicts:
-        return preds
-    else:
-        m_centered = np.zeros((numC,numC, numT, nSubj))
-        for ival, isubj in enumerate(subjs_list):
-            Xhat = preds[ival]
-            Xhat_centered = 0*Xhat.copy()
-            
-            for ic in range(numC): # here trials that match similar label orientation are shifted together y positions (np.roll)
-                Xhat_centered[:, G[ival] == ic,:] = np.roll(Xhat[:,G[ival] == ic,:], -ic, axis = 0)
-                m_centered[:,ic, :, ival] =  np.mean( Xhat_centered[:,  G[ival] == ic, :], axis = 1)
-
-        Xhat_centeredmean = np.mean( m_centered, axis = 1)
-        Xhat_centeredmean = np.mean( Xhat_centeredmean, axis = 2)
-        
-        return Xhat_centeredmean
     
 def train_main_ori(all_rawdata, raw_predicts = False, use_orientation = 0):
     '''Forward encoding model for the main task (6 orientations)'''
@@ -517,3 +552,57 @@ def train_timepoints_svc(X, y, verbose=False, display_roc=False, acc_only = True
     if acc_only:
         return accuracy
     return accuracy, f1, roc #, features
+
+def train_condv(all_rawdata, raw_predicts = False):
+    '''not working'''
+    nSubj = len(all_rawdata)
+    preds = [None] * nSubj
+    G = [None] * nSubj
+
+    for subj in range(nSubj):
+        
+        X = all_rawdata[subj]['epoch_dat']
+        if subj == 1: print(X.shape)
+        X = np.einsum('kji->jik', X)        
+        if subj == 1: print(X.shape)
+        
+        X = np.delete(X, 25, axis=0)
+        all_rawdata[subj]['metadata']['condv'] = 'C'
+        all_rawdata[subj]['metadata'].loc[all_rawdata[subj]['metadata']['cond']== 1, 'condv'] =  'D'
+
+        y = np.array(all_rawdata[subj]['metadata'].condv)
+        phi = y
+        numF, numT, numN = X.shape
+
+        G[subj] = y.copy() 
+        
+        CONDS = np.unique(G[subj])
+        nConds = CONDS.size
+        nfold = 5
+        FoldsIdx = dt.CreateFolds(G[subj], X, nfold)
+        
+        design, sortedesign = dt.stim_features(phi, cfg_stim)
+        
+        Xhat = np.zeros([numC,numN, numT])
+        for it in range(numT):
+            cfg = dict()
+            cfg['cfgE'] = {'gamma': 0.01, 'demean' : True, 'returnPattern' : True}
+            cfg['cfgD'] = {'demean' : 'traindata'}
+            Xhat[:,:,it] = dt.CV_encoder(design, X, it, cfg, FoldsIdx)
+        preds[subj] = Xhat
+    if raw_predicts:
+        return preds
+    else:
+        m_centered = np.zeros((numC,numC, numT, nSubj))
+        for ival, isubj in enumerate(subjs_list):
+            Xhat = preds[ival]
+            Xhat_centered = 0*Xhat.copy()
+            
+            for ic in range(numC): # here trials that match similar label orientation are shifted together y positions (np.roll)
+                Xhat_centered[:, G[ival] == ic,:] = np.roll(Xhat[:,G[ival] == ic,:], -ic, axis = 0)
+                m_centered[:,ic, :, ival] =  np.mean( Xhat_centered[:,  G[ival] == ic, :], axis = 1)
+
+        Xhat_centeredmean = np.mean( m_centered, axis = 1)
+        Xhat_centeredmean = np.mean( Xhat_centeredmean, axis = 2)
+        
+        return Xhat_centeredmean
